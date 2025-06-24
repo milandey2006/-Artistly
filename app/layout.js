f86ac4c649201f606997d4a4e2e33b2d4180c6aa
Milan Dey <96184592+milandey2006@users.noreply.github.com>
2025-06-24 20:41:22 +0530

    last update

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "Artistly - Connect with Talented Artists",
-  description: "Artistly is the premier platform connecting event planners with professional performing artists. Find the perfect talent for your event or showcase your skills to potential clients.",
+  description: "Artistly is the premier platform connecting event planners with professional performing artists. Find the perfect talent for your event or showcase your skills to potential clients. ",
 };
 
 export default function RootLayout({ children }) {
